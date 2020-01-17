1824c8300676ded81cdcce48ca45e0cc1553e91e
Opilane TTHK <opilane@tthk.ee>
2020-01-17 10:29:36 +0200

    Add project files.

---
+++ RaamatukoguMVC_Zurbenko/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RaamatukoguMVC_Zurbenko.MvcApplication" Language="C#" %>
---
+++ RaamatukoguMVC_Zurbenko/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RaamatukoguMVC_Zurbenko.Startup))]
+namespace RaamatukoguMVC_Zurbenko
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
